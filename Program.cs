ca6b28949bfb87eb7cabd0f2da0fd17a9f6c467f
unknown <abhisheksrivastava9719@gmail.com>
2022-06-14 16:18:59 +0530

    added UC3

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
 ﻿using Q_2;
 Console.WriteLine("Welcome to Welcome to Line Comparison Computation Program on Master Branch");
 
-double l1 = Class1.leng();
-double l2 = Class1.leng();
+double l1 = Class2.leng();
+double l2 = Class2.leng();
 if (l1 == l2)
     Console.WriteLine("Equal Lines");
+else if (l1 > l2)
+    Console.WriteLine("Line 1 greater than line 2");
 else
-    Console.WriteLine("Not Equal Lines");
+    Console.WriteLine("Line 2 is greater than line 1");
