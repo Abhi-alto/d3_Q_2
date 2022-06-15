879d764888e66fd4808927df44c6c0d3f53d7d81
unknown <abhisheksrivastava9719@gmail.com>
2022-06-15 15:58:17 +0530

    add

--- Class1.cs
+++ Class1.cs
@@ -8,5 +8,15 @@ namespace Q_2
 {
     internal class Class1
     {
+        Console.WriteLine("Enter x1");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Enter y1");
+            double y1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Enter x2");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Enter y2");
+            double y2 = Convert.ToDouble(Console.ReadLine());
+        double length = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            return length;
     }
 }
